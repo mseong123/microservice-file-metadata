e8b3354bd560b3f74ac7b30a198fdc255f2a86f2
Mseong <mseong123@hotmail.com>
2022-09-07 13:38:24 +0800

    done file metadata and uploaded to github to deploy to netlify

---
+++ netlify/functions/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+const serverless=require('serverless-http');
+const multer=require('multer');
+var cors = require('cors');
+const upload=multer() //Don't insert object parameter, hence keeping upload in memory only and not saved to permanent folder in server.
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+app.use(cors());
+app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+
+
+//API endpoint
+app.post('/api/fileanalyse', upload.single('upfile'),function (req, res) {
+  console.log(req.file)
+  res.json({
+    name:req.file.originalname,
+    type:req.file.mimetype,
+    size:req.file.size
+
+  })
+});
+
+
+module.exports.handler = serverless(app)
+
